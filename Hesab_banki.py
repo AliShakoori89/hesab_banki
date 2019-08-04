d39a1d356745b90183340b8c60f8ddbd6a7bb887
Ali Shakoori <alishakoori89@gmail.com>
2019-08-04 12:11:59 +0430

    add deposit method

--- Hesab_banki.py
+++ Hesab_banki.py
@@ -2,50 +2,69 @@ import json
 import os.path
 
 class Hesab_banki:
+    
+    def __init__(self,cus_name,cus_lastname,cus_id,cus_bank_acount_num,cus_supply):       
+        self.cus_name=cus_name
+        self.cus_lastname=cus_lastname
+        self.cus_id=cus_id
+        self.cus_bank_acount_num=cus_bank_acount_num
+        self.cus_supply=cus_supply
 
 
-    print('1. baz kardan hesab: ')
-    print('2.')
-    print('3.')
-    choise_num=str(input('Select the option you want:  '))
-    if choise_num==1:
-        #cus_name=str(input('Enter customer name: '))
-        #cus_lastname=str(input('Enter customer lastname: '))
-        #cus_id=str(input('Enter customer id: '))
-        #cus_bank_acount_num=str(input('Enter customer num: '))
-        #cus_supply=str(input('Enter supply: '))
-        def __init__(self,cus_name,cus_lastname,cus_id,cus_bank_acount_num,cus_supply):
-            self.mot1=str(input('Enter customer name: '))
-            self.mot2=str(input('Enter customer lastname: '))
-            self.mot3=str(input('Enter customer id: '))
-            self.mot4=str(input('Enter customer num: '))
-            self.mot5=str(input('Enter supply: '))         
-        __init__()
-        customer_obj = Hesab_banki(cus_name,cus_lastname,cus_id,cus_bank_acount_num,cus_supply)
+    def deposit (self,cus_sup):
+        #print(data[id_search]['suply'])
+        the_amount_of_curtains=int(input('How many sheets do you want to add: '))
+        data[id_search]['supply']=cus_sup+the_amount_of_curtains
+        with open('customer_list.json','w') as f:
+            f.write(json.dumps(data))
 
+        
 
+cus_specification={}
+all_cus_specification={}
+print('optins :')
+print('')
+print('1. add: ')
+print('2. deposit: ')
+choise_num=int(input('Enter your choise: '))
+if choise_num==1:
+    cus_name=str(input('Enter customer name: '))
+    cus_lastname=str(input('Enter customer lastname: '))
+    cus_id=str(input('Enter customer id: '))
+    cus_bank_acount_num=str(input('Enter customer bank acount num: '))
+    cus_supply=int(input('Enter supply: '))
+    customer_obj=Hesab_banki(cus_name,cus_lastname,cus_id,cus_bank_acount_num,cus_supply)
+    cus_specification={'Name':cus_name,'lastname':cus_lastname,'id':cus_id,'bank_account':cus_bank_acount_num,'supply':cus_supply}
+    all_cus_specification[cus_id]=cus_specification
+    if not os.path.isfile('customer_list.json'):
+        with open('customer_list.json','w')as f:
+            f.write(json.dumps(all_cus_specification))
+    else:
+        with open('customer_list.json')as f:
+            feeds=json.load(f)
+        feeds.update(all_cus_specification)
+        with open('customer_list.json','w') as f:
+            f.write(json.dumps(feeds))
+if choise_num==2:
+    id_search=str(input('Enter customer id: '))
+    with open('customer_list.json') as f:
+        data=json.load(f)
+        if id_search in data:
+            customer_obj=Hesab_banki(data[id_search]['Name'],data[id_search]['lastname'],data[id_search]['id'],data[id_search]['bank_account'],data[id_search]['supply'])
+            #print(data[id_search]['Name'],data[id_search]['lastname'],data[id_search]['id'],data[id_search]['bank_account'],data[id_search]['supply'])
+            #customer_select=Hesab_banki(data[id_search]['Name'],data[id_search]['lastname'],data[id_search]['id'],data[id_search]['bank_account'],data[id_search]['supply'])
+            #done=customer_obj.deposit(customer_select)
+            #print(data[id_search]['supply'])
+            cus_sup=data[id_search]['supply']
+            customer_obj.deposit(cus_sup)
+        else:
+            print('')
+            print('json file not exist id')
+    
+    
 
-
-
-
-#cus_specification={}
-        #all_cus_specification={}
-        #cus_name=
-        #cus_lastname=
-        #cus_id=
-        #cus_bank_acount_num=
-        #cus_supply=
-        #cus_specification={'Name':cus_name,'lastname':cus_lastname,'bank_account':cus_bank_acount_num,'supply':cus_supply}
-        #all_cus_specification[cus_id]=cus_specification
-
-        #if not os.path.isfile('customer_list.json'):
-         #   with open('customer_list.json','w')as f:
-         #       f.write(json.dumps(d))
-        #else:
-          #  with open('customer_list.json')as f:
-           #     feeds=json.load(f)
-            #feeds.update(d)
-            #with open('customer_list.json','w') as f:
-             #   f.write(json.dumps(feeds))
+            
+ 
+    
 
 
